487d88421ac13a1f2d00f110e91b74d7fe1d5ac5
andreyrsy <contatomainort@gmail.com>
2025-09-18 17:22:59 -0300

    feat: [Adicionando GlobalExceptionHandler para controlar as exceções]

---
+++ src/main/java/dev/andreyrsy/kitchen/flow/exception/GlobalExceptionHandler.java
@@ -0,0 +1,45 @@
+package dev.andreyrsy.kitchen.flow.exception;
+
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidacao(MethodArgumentNotValidException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", HttpStatus.BAD_REQUEST.value());
+        response.put("timestamp", LocalDateTime.now());
+        response.put("message", "Erro de validação nos campos");
+        response.put("erros", ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> Map.of("campo", Objects.requireNonNull(error.getField()), "mensagem", error.getDefaultMessage()))
+                .collect(Collectors.toList()));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<?> handleviolacao(ConstraintViolationException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", HttpStatus.BAD_REQUEST.value());
+        response.put("timestamp", LocalDateTime.now());
+        response.put("message", "Erro de validação nos campos");
+        response.put("erros", ex.getConstraintViolations()
+                .stream()
+                .map(error -> Map.of("campo", error.getPropertyPath().toString(), "mensagem", error.getMessage()))
+                .collect(Collectors.toList()));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+}
