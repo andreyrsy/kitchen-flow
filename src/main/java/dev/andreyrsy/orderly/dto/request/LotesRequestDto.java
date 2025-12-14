e8f432e4982bd5670da0cc43c7fba1aa0e646ede
andreyrsy <contatomainort@gmail.com>
2025-12-14 10:59:46 -0300

    refactor(dto): update LotesRequestDTO

--- src/main/java/dev/andreyrsy/orderly/dto/request/LotesRequestDto.java
+++ src/main/java/dev/andreyrsy/orderly/dto/request/LotesRequestDto.java
@@ -1,6 +1,7 @@
 package dev.andreyrsy.orderly.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -11,10 +12,12 @@ public class LotesRequestDto {
     @NotNull(message = "A quantidade deve ser informada")
     private Integer quantidade;
 
+    @JsonProperty("dataEntrada")
     @NotNull(message = "A data de entrada deve ser informada")
     @JsonFormat(pattern = "dd-MM-yyyy", shape = JsonFormat.Shape.STRING)
     private LocalDate dataEntrada;
 
+    @JsonProperty("dataValidade")
     @NotNull(message = "A data de validade deve ser informada")
     @JsonFormat(pattern = "dd-MM-yyyy", shape = JsonFormat.Shape.STRING)
     private LocalDate dataValidade;
